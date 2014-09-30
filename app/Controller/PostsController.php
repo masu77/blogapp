8005069c662d00c8600e4986bcc843c541b1a6cd
Nagamine <nagamine@facilis.info>
2014-09-30 03:54:59 +0000

    ステップを実装しフィーチャテストが通過するようにした

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -14,6 +14,10 @@ class PostsController extends AppController {
 	];
 
 	public function index() {
+        $this->Paginator->settings = [
+            'limit' => 5,
+            'order' => ['Post.id' => 'desc'],
+        ];
 		$this->set('posts', $this->Paginator->paginate());
 	}
 	
