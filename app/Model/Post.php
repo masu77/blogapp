39874007007a59118e4f217e14b7093692e387c8
Nagamine <nagamine@facilis.info>
2014-10-01 05:38:37 +0000

    DocBlockコメントを追加

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,28 +1,41 @@
 <?php
 App::uses('AppModel', 'Model');
+
 /**
  * Post Model
+ *
+ * ブログ記事用モデルです
+ *
+ * @copyright php_ci_book
+ * @link https://github.com/phpcibook/blogapp/blob/master/app/Model/Post.php
+ * @since 1.0
+ * @auther 作成者の名前 <test@example.com>
  *
  */
 class Post extends AppModel {
 
 /**
- * Validation rules
+ * 一覧表示時のタイトルに使用するカラム名
+ *
+ * @var string
+ */
+	public $displayField = 'title';
+
+/**
+ * バリデーションルール
  *
  * @var array
  */
-	public $validate = array(
+	public $validate = [
 		'title' => [
 			'notEmpty' => [
 				'rule' => ['notEmpty'],
 				'message' => 'タイトルは必須入力です',
 			],
 			'maxLength' => [
-				'rule' => ['maxLength', 255],
+				'rule' => ['maxLength', '255'],
 				'message' => 'タイトルは255文字以内で入力してください',
 			],
-
 		],
-
-	);
+	];
 }
